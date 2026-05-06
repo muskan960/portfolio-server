173f13df6accf6fe67c5322c731663b4a6d12527
muskan123412 <zahidanadeem753@gmail.com>
2026-05-06 17:06:03 +0500

    added new update in backend

--- server.js
+++ server.js
@@ -1,24 +1,5 @@
 
 
-// require("dotenv").config();
-
-// const express = require("express");
-// const cors = require("cors");
-
-// const app = express();
-
-// app.use(cors());
-// app.use(express.json());
-
-// app.use("/api", require("./routes/userRoutes"));
-
-// const PORT = process.env.PORT || 4000;
-
-// app.listen(PORT, () => {
-//   console.log(`🚀 Server running on port ${PORT}`);
-// });
-
-
 require("dotenv").config();
 
 const express = require("express");
@@ -26,15 +7,14 @@ const cors = require("cors");
 
 const app = express();
 
-// Middleware
-app.use(cors({
-  origin: "*", // ya apna frontend URL dal sakti ho
-}));
+app.use(cors());
 app.use(express.json());
 
-// Routes
 app.use("/api", require("./routes/userRoutes"));
 
-// ❌ listen hata diya
-// ✅ export for Vercel
-module.exports = app;
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
+
